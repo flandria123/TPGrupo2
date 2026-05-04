380881f34927566a6ee344c1f34e1b036866f511
Chris-Jackson-repo <christian.jackson033@gmail.com>
2026-05-04 18:01:05 -0300

    Agrego API usuarios y la entidad en Models
    Aparecen unos cambios también sobre los nu get

---
+++ src/Users.API/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Users.API.Models
+{
+    public class Item
+    {
+
+        public Guid Id { get; init; }
+        [Required] public string Name { get; init; }
+        [Required] public string Apellido { get; init; }
+        [Required] public string Email { get; init; }
+        public string PasswordHash { get; init; }
+        public DateTime FechaRegistro { get; init; }
+        public bool Activo { get; set; } = true;
+        public int IntentosFallidos { get; set; } = 0;
+
+
+
+
+    }
+
+    
+
+
+
+}
+
