ef55c6c06ff5bfae791e06bf28dfc89716e8f92e
Chris-Jackson-repo <christian.jackson033@gmail.com>
2026-06-04 20:12:14 -0300

    Handlers
    Agrego ExceptionHandlers

---
+++ src/Cart.API/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc; 
+
+namespace CartAPI.ExceptionHandlers
+{
+    
+    public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext context,
+            Exception exception,
+            CancellationToken cancellationToken)
+        {
+            
+            logger.LogError(exception, "Ocurrió un error no manejado en Cart.API: {Mensaje}", exception.Message);
+
+            
+            var statusCode = StatusCodes.Status500InternalServerError;
+            context.Response.StatusCode = statusCode;
+
+            
+            var correlationId = context.Request.Headers["X-Correlation-Id"].FirstOrDefault()
+                                ?? context.TraceIdentifier;
+
+            
+            await context.Response.WriteAsJsonAsync(new
+            {
+                type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+                title = "Internal Server Error",
+                status = statusCode,
+                detail = "Ocurrió un error inesperado en el servidor.",
+                instance = context.Request.Path.Value,
+                errorCode = "CRT-005", 
+                errorMessage = "Error interno al procesar el carrito.", 
+                correlationId = correlationId
+            }, cancellationToken: cancellationToken);
+
+           
+            return true;
+        }
+    }
+}
---
+++ src/Cart.API/ExceptionHandlers/ValidationExceptionHandler.cs
@@ -0,0 +1,44 @@
+﻿using CartAPI.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc; // Para StatusCodes
+
+namespace CartAPI.ExceptionHandlers
+{
+    public class ValidationExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(
+            HttpContext context,
+            Exception exception,
+            CancellationToken cancellationToken)
+        {
+            
+            if (exception is not ValidationException ex)
+            {
+                return false; 
+            }
+
+           
+            var statusCode = StatusCodes.Status400BadRequest;
+            context.Response.StatusCode = statusCode;
+
+            
+            var correlationId = context.Request.Headers["X-Correlation-Id"].FirstOrDefault()
+                                ?? context.TraceIdentifier;
+
+           
+            await context.Response.WriteAsJsonAsync(new
+            {
+                type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                title = "Bad Request",
+                status = statusCode,
+                detail = "Los datos ingresados son inválidos.",
+                instance = context.Request.Path.Value,
+                errorCode = ex.ErrorCode,
+                errorMessage = ex.Message,
+                correlationId = correlationId 
+            }, cancellationToken: cancellationToken);
+
+            return true; 
+        }
+    }
+}
